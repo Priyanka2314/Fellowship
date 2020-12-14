74bcb1eb61f18025ed6571984a9b4bcefb4c5907
Priyanka <priyankacs102@gmail.com>
2020-12-14 10:46:18 +0530

    Refraction the code to write a function to get work hours

--- empWage.js
+++ empWage.js
@@ -1,5 +1,5 @@
 //UseCase1 IF Else condition
-{
+/*{
 const IS_ABSENT=0;
 let empcheck= Math.floor(Math.random() * 10) % 2;
 if(empcheck == IS_ABSENT)
@@ -7,30 +7,31 @@ console.log("UC1 - Employee is Absent. Existing the program");
 else
 console.log("UC1 - Employee is Present");
 }
+*/
 
-//UC2 Switch Case
+//refracting the UC2 Switch Case code for UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-{
-  let empHrs = 0;
-  let empcheck = Math.floor(Math.random() * 10) % 3;
-  switch(empcheck){
+  
+  function getWorkingHours(empCheck){
+  switch(empCheck){
       case IS_PART_TIME:
-          empHrs = PART_TIME_HOURS;
-          break;
+          return PART_TIME_HOURS;
       case IS_FULL_TIME:
-          empHrs = FULL_TIME_HOURS; 
-          break;
+        return FULL_TIME_HOURS; 
       default :
-          empHrs=0;
-  }
-  let empWage=empHrs* WAGE_PER_HOUR;
-  console.log("UC2 - Emp Wage: "+ empWage);
-
+      return 0;
+    }
 }
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  let empHrs = getWorkingHours(empCheck);
+  let empWage=empHrs * WAGE_PER_HOUR;
+  console.log("Hours "+empHrs + " Emp Wage: "+ empWage);
+
+
 
 
 
