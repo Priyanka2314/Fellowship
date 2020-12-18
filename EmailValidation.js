c4b3ff5c7777e74cd96a7ff3e13a67dae54876e8
Priyanka <priyankacs102@gmail.com>
2020-12-18 06:05:07 +0530

    UC2 validating second  part of abc.xyz@bridgelabz.co.in

--- EmailValidation.js
+++ EmailValidation.js
@@ -1,12 +1,15 @@
 /**
  * email format : abc.xyz@bridgelabz..co.in
  * UC1 validating firt part of the email i.e. abc
- * 
+ * UC2 validating second part of the email pattern
+ * UC3 validating third part .co in email
  */
 const user_input = require('readline-sync');
 let emailRegEx = RegExp("^[a-z]{3,}");
+let emailRegEx2 = RegExp("^[a-z]{3,}[@][a-z]{2,}$");
+
 function emailVaildation(email){
-    if(emailRegEx.test(email)){
+    if(emailRegEx2.test(email)){
         console.log(email+" is valid");
         return true;
     }
